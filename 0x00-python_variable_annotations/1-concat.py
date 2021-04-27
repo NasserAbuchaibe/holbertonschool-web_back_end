0e7b6dd0ce4863afc96bf5589a8d81f5b3d814f2
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2021-04-26 19:12:04 -0500

    1. Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Write a type-annotated function concat that takes a string str1 and a
+    string str2 as arguments and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """[Concat two str]
+
+    Args:
+        str1 (str): [string 1]
+        str2 (str): [string 2]
+
+    Returns:
+        str: [concatenated string]
+    """
+    return str1 + str2
