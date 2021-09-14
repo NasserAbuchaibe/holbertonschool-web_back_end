ef1ae2b389034b2ba33566f3b4e0d47c47af194e
NasserAbuchaibe <nasser_abuchaibe@hotmail.com>
2021-09-14 17:39:08 -0500

    task 7-100

---
+++ 0x0F-ES6_promise/100-await.js
@@ -0,0 +1,20 @@
+import { createUser, uploadPhoto } from "./utils";
+
+const asyncUploadUser = async () => {
+  let obj = {
+    photo: null,
+    user: null,
+  };
+
+  try {
+    obj = {
+      photo: await uploadPhoto(),
+      user: await createUser(),
+    };
+    return obj;
+  } catch (error) {
+    return obj;
+  }
+};
+
+export default asyncUploadUser;
